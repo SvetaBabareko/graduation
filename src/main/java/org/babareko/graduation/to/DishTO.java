7552902763dd5163c7494984eca187985d429f7e
Светлана <sveta.romashko@inbox.ru>
2021-05-25 07:02:39 +0300

    add TO layer

---
+++ src/main/java/org/babareko/graduation/to/DishTO.java
@@ -0,0 +1,13 @@
+package org.babareko.graduation.to;
+
+public class DishTO {
+    private final Integer id;
+    private final String name;
+    private final Integer price;
+
+    public DishTO(Integer id, String name, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
--- src/main/java/org/babareko/graduation/to/RestaurantTO.java
+++ src/main/java/org/babareko/graduation/to/RestaurantTO.java
@@ -1,4 +1,13 @@
 package org.babareko.graduation.to;
 
 public class RestaurantTO {
+    private final Integer id;
+    private final String name;
+    private final String description;
+
+    public RestaurantTO(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
